6088cac597a175e6a5cb2c70f01a5e982ea3a7d7
emanuelcondo <emanuelcondo@gmail.com>
2015-10-01 14:55:23 -0300

    verificación de ejecutables. (Falta válidar permisos)

--- AFINI.sh
+++ AFINI.sh
@@ -9,12 +9,12 @@
 function inicializarAmbiente {
 
 	# CASO 1 : AMBIENTE NO INICIALIZADO
-	if [ -z $fueInicializado ]; then
+	if [ -z $MAEDIR ]; then
 		# Si no existe el archivo de configuración, volver a instalar
 		if [ ! -f "conf/AFINSTAL.cnfg" ]; then
 			echo "No se encontro archivo de configuración AFINSTAL.cnfg"
-			echo "Volvel a instalar"
-			# Agregar lo del GraLog : No se encontró archivo de configuración
+			echo "Vuelva a realizar la instalación"
+			bash ./GraLog.sh AFINI EF "No se encontró el archivo de configuración."
 			return 1
 		else
 			# Si existe, verificar permisos de lectura
@@ -22,13 +22,14 @@ function inicializarAmbiente {
 				chmod +r "conf/AFINSTAL.cnfg"
 				if [ ! -r "conf/AFINSTAL.cnfg" ]; then
 					echo "El archivo AFINSTAL.cnfg no tiene permisos de lectura."
-					# Agregar lo del GraLog : El archivo de configuración no tiene permisos de lectura
+					bash ./GraLog.sh AFINI E "El archivo de configuración no tiene permisos de lectura."
 					return 2
 				fi
 			fi
 		fi
 
 		echo "Inicializando variables de ambiente..."
+		oldIFS=$IFS
 		IFS=$'\n'
 		# Seteo de variables de ambiente
 		for linea in $(< "conf/AFINSTAL.cnfg")
@@ -41,23 +42,22 @@ function inicializarAmbiente {
 				PATH="$PATH:$valor"	
 			fi	
 		done
-
-		export fueInicializado=true
+		IFS=$oldIFS
 		sleep 1
 		echo "Ambiente Inicializado : OK"
-		# Agregar GraLog : Inicialización de ambiente correctamente
+		bash ./GraLog.sh AFINI I "El ambiente ha sido inicializado correctamente."
 
 	# CASO 2 : AMBIENTE INICIALIZADO
 	else
 		echo "Ambiente ya inicializado."
 		echo "Para reiniciar, termine la sesión e ingrese nuevamente."
-		# Agregar GraLog : Se quiere inicializar ambiente ya inicializado
+		bash ./GraLog.sh AFINI W "Se quiere inicializar ambiente ya inicializado."
 		echo "¿Desea terminar la sesión? (s-n)"
 		read terminar
 
 		if [ $terminar == 's' ]; then
 			echo "Cerrando Sesión"
-			# Agregar GraLog : Usuario cierra sesión
+			bash ./GraLog.sh AFINI I "Usuario cierra sesión."
 			return 3
 		else
 			if [ $terminar == 'n' ]; then
@@ -77,28 +77,28 @@ function inicializarAmbiente {
 # 0: Instalación Completa
 # 1: Faltante de Archivos (Scripts, Maestros, Tablas, etc.)
 function instalacionCompleta {
-	todoOk=0
+	instalacionOk=0
 	echo "Verificando Scripts..."
 	sleep 1
 	existenScripts
 	ret=$?
-	if [ $ret -gt 0 ]; then
-		todoOk=1
+	if [ $ret -ne 0 ]; then
+		instalacionOk=1
 	fi
 	echo "Verificando Maestros y Tablas..."
 	sleep 1
 	existenMaestrosYTablas
 	ret=$?
-	if [ $ret -gt 0 ]; then
-		todoOk=1
+	if [ $ret -ne 0 ]; then
+		instalacionOk=1
 	fi
 
-	if [ $todoOk -eq 1 ]; then
+	if [ $instalacionOk -ne 0 ]; then
 		echo "Instalación Incompleta. Por favor, vuelta a realizar la instalación e intente nuevamente."
-		# GraLog
+		bash ./GraLog.sh AFINI EF "Instalacion incompleta."
 		return 1
 	fi
-	
+
 	return 0
 }
 
@@ -106,7 +106,23 @@ function instalacionCompleta {
 # 0: Existen todos los scripts correspondientes
 # 1: Faltante de algún script
 function existenScripts {
-	return 0	
+	todoOk=0
+	# chequeo si existe archivo ejecutable MoverA.sh
+	if [ ! -f "$BINDIR/MoverA.sh" ]; then
+		todoOk=1
+		echo " Archivo Ejecutable MoverA: NO ENCONTRADO"
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo ejecutable MoverA."
+	fi
+
+	# chequeo si existe archivo ejecutable AFREC.sh
+	if [ ! -f "$BINDIR/AFREC.sh" ]; then
+		todoOk=1
+		echo " Archivo Ejecutable AFREC: NO ENCONTRADO"
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo ejecutable AFREC."
+	fi	
+
+	# Agregar los ejecutables que falten
+	return $todoOk	
 }
 
 #retorno de valores
@@ -118,42 +134,42 @@ function existenMaestrosYTablas {
 	if [ ! -f "$MAEDIR/CdP.mae" ]; then
 		todoOk=1
 		echo " Archivo Maestro de Código de País: NO ENCONTRADO"
-		# GraLog
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo maestro CdP.mae ."
 	fi
 
 	# chequeo si existe archivo Maestro de Código de Area de Argentina
 	if [ ! -f "$MAEDIR/CdA.mae" ]; then
 		todoOk=1
 		echo " Archivo Maestro de Código de Area de Argentina: NO ENCONTRADO"
-		# GraLog
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo maestro CdA.mae ."
 	fi
 
 	# chequeo si existe archivo Maestro de Centrales
 	if [ ! -f "$MAEDIR/CdC.mae" ]; then
 		todoOk=1
 		echo " Archivo Maestro de Código de Centrales: NO ENCONTRADO"
-		# GraLog
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo maestro CdC.mae ."
 	fi
 
 	# chequeo si existe archivo Maestro de Agentes
 	if [ ! -f "$MAEDIR/agentes.mae" ]; then
 		todoOk=1
 		echo " Archivo Maestro de Código de Agentes: NO ENCONTRADO"
-		# GraLog
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo maestro agentes.mae ."
 	fi
 
 	# chequeo si existe tabla de Tipos de Llamadas
 	if [ ! -f "$MAEDIR/tllama.tab" ]; then
 		todoOk=1
 		echo " Tabla de llamadas: NO ENCONTRADO"
-		# GraLog
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo maestr tllama.tab ."
 	fi
 
 	# chequeo si existe tabla de Umbrales de Consumo
 	if [ ! -f "$MAEDIR/umbral.tab" ]; then
 		todoOk=1
 		echo " Tabla de Umbrales de Consumo: NO ENCONTRADO"
-		# GraLog
+		bash ./GraLog.sh AFINI EF "No se encontró el archivo maestro ubral.tab ."
 	fi
 
 	return $todoOk
@@ -170,11 +186,13 @@ function listarArchivosDir {
 				echo "  $file"
 			fi
 		done
+		cd ..
 	fi	
 }
 
 
 function verificarPermisos {
+	dir=$MAEDIR
 	echo "Verificando permisos..."
 	sleep 1
 	return 0
@@ -190,58 +208,68 @@ function mostrarYgrabar {
 	echo "Directorio de Configuración: $CONFDIR"
 	sleep 1
 	listarArchivosDir $CONFDIR
+	bash ./GraLog.sh AFINI I "CONFDIR = $CONFDIR"
 	#Grabar en el log
 	echo
 
 	echo "Directorio de Ejecutables: $BINDIR"
 	sleep 1
 	listarArchivosDir $BINDIR
+	bash ./GraLog.sh AFINI I "BINDIR = $BINDIR"
 	#Grabar en el log
 	echo
 
 	echo "Directorio de Maestros y Tablas: $MAEDIR"
 	sleep 1
 	listarArchivosDir $MAEDIR
+	bash ./GraLog.sh AFINI I "MAEDIR = $MAEDIR"
 	#Grabar en el log
 	echo
 
 	echo "Directorio de recepción de archivos de llamadas: $NOVEDIR"
 	sleep 1
 	listarArchivosDir $NOVEDIR
+	bash ./GraLog.sh AFINI I "NOVEDIR = $NOVEDIR"
 	#Grabar en el log
 	echo
 
 	echo "Directorio de Archivos de llamadas Aceptadas: $ACEPDIR"
 	sleep 1
 	listarArchivosDir $ACEPDIR
+	bash ./GraLog.sh AFINI I "ACEPDIR = $ACEPDIR"
 	#Grabar en el log
 	echo
 
 	echo "Directorio de Archivos de llamadas Sospechosas: $PROCDIR"
 	sleep 1
 	listarArchivosDir $PROCDIR
+	bash ./GraLog.sh AFINI I "PROCDIR = $PROCDIR"
 	#Grabar en el log
 	echo
 
 	echo "Directorio de Archivos de Reportes de llamadas: $REPODIR"
 	sleep 1
 	listarArchivosDir $REPODIR
+	bash ./GraLog.sh AFINI I "REPODIR = $REPODIR"
 	#Grabar en el log
 	echo
 	
 	echo "Directorio de Archivos de Log: $LOGDIR"
 	sleep 1
 	listarArchivosDir $LOGDIR
+	bash ./GraLog.sh AFINI I "LOGDIR = $LOGDIR"
 	#Grabar en el log
 	echo
 	
 	echo "Directorio de Archivos Rechazados: $RECHDIR"
 	sleep 1
 	listarArchivosDir $RECHDIR
+	bash ./GraLog.sh AFINI I "RECHDIR = $RECHDIR"
 	#Grabar en el log
 	echo
 
 	echo "Estado del Sistema: INICIALIZADO"
+	bash ./GraLog.sh AFINI I "Grabación de variables correctamente."
 	#Grabar en el log : OK
 	sleep 1
 
@@ -258,11 +286,11 @@ function arrancarAFREC {
 			echo "Iniciando AFREC..."
 			sleep 1
 			echo "AFREC corriendo bajo el no.: <Process Id de AFREC>"
-			# llamar a AFREC
+			#TODO llamar a AFREC
 			exit
 		else
 			echo "WARNING: Ya hay un proceso AFREC corriendo."
-			# GraLog
+			bash ./GraLog.sh AFINI W "Ya hay un proceso AFREC corriendo."
 			exit
 		fi
 	else
