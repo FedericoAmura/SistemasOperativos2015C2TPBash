7736ea89a50e33f34a22f0e9e73db408213d4a80
popota <mauro.com9@gmail.com>
2015-10-02 00:35:40 -0300

    Agrego una nueva version de compose.sh Para crear el paquete de instalacion solo tienen que escribir en una terminal
    $ bash compose.sh -pack
    
    Para saber mas sobre la implementacion o como es el arbol de direcorios creados
    mirar el codigo del script "compose.sh"
    
    	modified:   compose.sh
    	new file:   data/BEL_20150703
    	new file:   data/BEL_20150803
    	new file:   data/COS_20150703
    	new file:   data/COS_20150803
    	new file:   data/SIS_20150703.csv
    	new file:   data/SIS_20150803.csv
    	new file:   data/archivoDeLlamadasSospechosas
    	new file:   data/co_central.rech

--- compose.sh
+++ compose.sh
@@ -9,10 +9,42 @@
 #
 
 
+# Crea archivo .zip (paquete a descargar por el usuario)
+# Directorio:
+#
+#├── AFRA-J
+#│   ├── AFINSTALL.sh
+#│   ├── bin
+#│   │   ├── AFINI.sh
+#│   │   ├── AFREC.sh
+#│   │   ├── GraLog.sh
+#│   │   ├── movera_old.sh
+#│   │   └── MoverA.sh
+#│   ├── conf
+#│   │   ├── AFINSTAL.cnfg
+#│   │   └── AFINSTAL.lg
+#│   └── data
+#│       ├── agentes.csv
+#│       ├── BEL_20150703.csv
+#│       ├── BEL_20150803.csv
+#│       ├── CdA.csv
+#│       ├── CdP.csv
+#│       ├── centrales.csv
+#│       ├── COS_20150703.csv
+#│       ├── COS_20150803.csv
+#│       ├── SIS_20150703.csv
+#│       ├── SIS_20150803.csv
+#│       └── umbrales.csv
+# 
+#
+
+
+
+
 #Set Name
 NOMBRE_PAQUETE_ZIP="AFRA-J.zip"
 #Variable de entorno para usar en AFINSTAL.sh
-PAQUETE="AFRA-J"
+GRUPO="AFRA-J"
 
 #Set Archivos
 
@@ -22,18 +54,40 @@ if [ "$1" == "-pack" ]
 then
 	
 	#Carpeta par contener el paquete y arbol de directorio por defecto.
-	mkdir $PAQUETE
-	mkdir $PAQUETE/conf
-	
+	mkdir $GRUPO
+	mkdir $GRUPO/conf
+	mkdir $GRUPO/bin
+	mkdir $GRUPO/data
+
+
 	#Agrega todos los scripts para la instalacion.
-	cp AFINSTALL.sh $PAQUETE/
+	cp AFINSTALL.sh $GRUPO/
+	#Agrego el resto de los *.sh al direcorio /bin	
+
+	cp AFINI.sh $GRUPO/bin/
+	cp AFREC.sh $GRUPO/bin/
+	cp GraLog.sh $GRUPO/bin/
+	cp MoverA.sh $GRUPO/bin/
+
+
+	#Agrego el archivo readme
+	cp README.md $GRUPO/
+
+	#Agrego el resto de los documentos a /data
+	cp -R data $GRUPO/
+
 	
+	#Agrego arhivos de configuracion en /conf
+	> $GRUPO/conf/AFINSTAL.cnfg
+	> $GRUPO/conf/AFINSTAL.lg
+
+
 	#Crea archivo .zip
-	zip -r $NOMBRE_PAQUETE_ZIP $PAQUETE 	
+	zip -r $NOMBRE_PAQUETE_ZIP $GRUPO 	
 	
 	#Con AFRA-J.zip creado
 	#Elimina todo el directorio AFRA-J
-	rm -R $PAQUETE 
+	rm -R $GRUPO 
 
 	echo -e "Se creo el paquete de instalacion \n \t PATH: $PWD/$NOMBRE_PAQUETE_ZIP"
 	
