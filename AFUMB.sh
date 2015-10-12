ab44bf409684d3eaa2d40a3c25c5f5acc3af7516
carolinarau <rau.carolina@gmail.com>
2015-10-12 02:48:52 -0300

    Hasta 4.2.1 - Ahora si valida los registros

--- AFUMB.sh
+++ AFUMB.sh
@@ -23,61 +23,314 @@
 source MoverA.sh
 source GraLog.sh
 LOGDIR="$PWD/log"
-LOGSIZE=50
+LOGSIZE=100
 LOGEXT=log
 
-validarRegistro() {
-	registro=$1
-	#valido agente
-	validaAgente=$(validarAgente $registro) 
-	if [ $validaAgente -eq 0 ]
+
+validarAgente() {
+
+	
+	#buscar el agente en el maestro de agentes.
+	existe=$(grep "$1" "$MAE_AGENTES") 
+	agente_encontrado=$(echo "$existe" | cut -d ';' -f3)
+	if [ "$agente" != "$agente_encontrado" ]
+	then
+		GraLog AFUMB INFO "Agente invalido: $agente"
+		echo 1
+		return
+	fi
+	#valido ok
+	echo 0
+
+}
+
+validarArea() {
+
+	#buscar area en el maestro de codigo de areas
+	area=$1
+	existe_area=$(grep -w "$area" "$MAE_COD_AREA_ARG") 
+	area_encontrada=$(echo "$existe_area" | cut -d ';' -f2)
+	if [ ! -z $area_encontrada ]
 	then	
-		validaArea=$(validarArea $registro)
-		if [ $validaArea -eq 0 ]
+		if [ $area -ne $area_encontrada ]
 		then
-			echo 0
-			return		
-		else
-			echo 1
+			echo 1 
 			return
 		fi
 	else
 		echo 1
 		return
 	fi
-}
-
-validarAgente() {
-
-	agente=$(echo "$1" | cut -d';' -f1)
-	#buscar el agente en el maestro de agentes.
-	existe=$(grep "$agente" "$MAE_AGENTES") 
-	agente_encontrado=$(echo "$existe" | cut -d ';' -f 3)
-	if [ "$agente" != "$agente_encontrado" ]
-	then
-		echo 1
-		return
-	fi
-	#valido ok  
+	#area valida
 	echo 0
 }
 
-validarArea() {
-	area=$(echo "$1" | cut -d';' -f4)
-	#buscar area en el maestro de codigo de areas
-	existe_area=$(grep -w "$area" "$MAE_COD_AREA_ARG") 
-	area_encontrada=$(echo "$existe_area" | cut -d ';' -f 2)
-	if [ "$area" = "$area_encontrada" ]
-	then
-		echo 0
+validarNumeroLineaA() {
+	
+	longArea=${#1}
+	longlinea=${#2}
+	linea_ok=$(validarLineaA $longArea $longlinea)
+	if [ $linea_ok -ne 0 ]
+	then 
+		GraLog AFUMB INFO "Linea A invalido: $1"	
+		echo 1
 		return
 	fi
-	echo 1
+	echo 0
 }
+
+validarNumeroLineaB() {
+	
+	#validar tipo de llamada
+	TIPO_LLAMADA=$(determinarTipoLlamada "$1")
+	NUMLINEA=$(echo "$1" | cut -d';' -f8)
+	if [ "$TIPO_LLAMADA" = "DDI" ]
+	then	
+		COD_AREA_PAIS=$(echo "$1" | cut -d';' -f6)
+		GraLog AFUMB INFO "Codigo area pais: $COD_AREA_PAIS"
+		if [ ! -z $COD_AREA_PAIS ]
+		then
+			COD_AREA_PAIS_OK=$(validarCodPais $COD_AREA_PAIS)
+			if [ $COD_AREA_PAIS_OK -eq 0 ]
+			then
+				#Como la llamada es DDI no tiene codigo de area
+				#valido la linea
+				GraLog AFUMB INFO "NUMLINEA: $NUMLINEA"
+				#Agregar expresion regular que valide que sea un numero - TODO
+				echo 0
+				return
+			
+			else
+				GraLog AFUMB INFO "Codigo area pais invalido: $COD_AREA_PAIS"
+				echo 1
+				return
+			fi
+		fi
+	else
+		if [ "$TIPO_LLAMADA" = "DDN" ] || [ "$TIPO_LLAMADA" = "LOC" ]	
+		then
+			COD_AREA=$(echo "$1" | cut -d';' -f7)
+			GraLog AFUMB INFO "COD_AREA: $COD_AREA"
+			#Valido el codigo de area
+			COD_AREA_OK=$(validarArea $COD_AREA)
+			if [ $COD_AREA_OK -eq 0 ] 
+			then
+				
+				#Area valida		
+				#Tipo de llamada LOCAL o DDN			
+				AREA_LINEA=$COD_AREA$NUMLINEA
+				GraLog AFUMB INFO "AREA_LINEA: $AREA_LINEA"
+				LONG_AREA_LINEA=${#AREA_LINEA}
+				if [ $LONG_AREA_LINEA -eq 10 ]
+				then
+					GraLog AFUMB INFO "Linea B VALIDA: $NUMLINEA"
+					echo 0
+					return
+				else
+
+					GraLog AFUMB INFO "Linea B invalido: $NUMLINEA"
+					echo 1
+					return
+				fi
+			else 
+
+				GraLog AFUMB INFO "Codigo area invalido: $COD_AREA"
+				echo 1
+				return
+
+			fi
+		else
+			GraLog AFUMB INFO "Linea B invalido: $NUMLINEA"
+			echo 1
+			return
+		fi
+	fi
+}
+
+validarCodPais() {
+	
+	EXISTE_COD_AREA_PAIS=$(grep -w "$1" "$MAE_COD_PAIS") 
+	COD_AREA_PAIS_ENC=$(echo "$EXISTE_COD_AREA_PAIS" | cut -d ';' -f1)
+	if [ $COD_AREA_PAIS -ne $COD_AREA_PAIS_ENC ] 
+	then
+		GraLog AFUMB INFO "Codigo pais invalido: $COD_AREA_PAIS"
+		echo 1
+		return
+	fi
+	echo 0
+}
+
+validarTiempoConversacion() {
+
+	TIEMPO=$(echo "$1" | cut -d';' -f3)
+	if [ $TIEMPO -lt 0 ]
+	then
+		GraLog AFUMB INFO "Tiempo de conversacion invalido: $TIEMPO"
+		echo 1
+		return
+	fi
+	echo 0
+}
+
+validarLineaA() {
+
+#$1 longArea
+#$2 longlinea
+
+	if [ $1 -eq 2 ]
+	then
+		#linea 8 digitos
+		if [ $2 -ne 8 ]
+		then
+			#linea invalida
+			GraLog AFUMB INFO "linea invalida: $2"
+			echo 1
+			return
+		fi
+	else
+		if [ $1 -eq 3 ]
+		then
+			#linea 7 digitos
+			if [ $2 -ne 7 ]
+			then
+				#linea invalida
+				GraLog AFUMB INFO "linea invalida: $2"
+				echo 1
+				return
+			fi
+		else
+			#linea 6 digitos
+			if [ $2 -ne 6 ]
+			then
+				#linea invalida
+				GraLog AFUMB INFO "linea invalida: $2"
+				echo 1
+				return
+			fi
+		fi
+	fi
+	#linea valida
+	echo 0
+}
+
+determinarTipoLlamada() {
+
+	REGISTRO=$1
+	COD_AREA_PAIS=$(echo "$REGISTRO" | cut -d';' -f6)
+	if [ ! -z $COD_AREA_PAIS ]
+	then
+		GraLog AFUMB INFO "EXISTE CODIGO AREA PAIS"		
+		COD_AREA_PAIS_OK=$(validarCodPais $COD_AREA_PAIS)
+		GraLog AFUMB INFO "COD_AREA_PAIS: $COD_AREA_PAIS"
+		LINEA_B=$(echo "$REGISTRO" | cut -d';' -f8)
+		GraLog AFUMB INFO "LINEA_B: $LINEA_B"
+		GraLog AFUMB INFO "COD_AREA_PAIS_OK: $COD_AREA_PAIS_OK"
+		if [ $COD_AREA_PAIS_OK -eq  0 ]  &&  [ ! -z $LINEA_B ]
+		then
+			#tipo de llamada DDI
+			GraLog AFUMB INFO "TIPO DE LLAMADA DDI"	
+			echo "DDI"
+			return
+		else
+			echo "INV"
+			return 
+		fi
+	else
+		GraLog AFUMB INFO " NO EXISTE CODIGO AREA PAIS"
+		LINEA_A=$(echo "$REGISTRO" | cut -d';' -f5)
+		COD_AREA_A=$(echo "$REGISTRO" | cut -d';' -f4)
+		COD_AREA_B=$(echo "$REGISTRO" | cut -d';' -f7)
+		LONG_AREA=${#COD_AREA_A}
+		LONG_LINEA=${#LINEA_A}
+		LINEA_A_OK=$(validarLineaA $LONG_AREA $LONG_LINEA)	
+		if [ $COD_AREA_A -ne $COD_AREA_B ] && [ $LINEA_A_OK -eq 0 ]
+		then
+			#tipo de llamada DDN
+			GraLog AFUMB INFO "TIPO LLAMADA DDN"
+			echo "DDN"
+			return
+		else
+			if [ $COD_AREA_A -eq $COD_AREA_B ] && [ $LINEA_A_OK -eq 0 ]
+			then
+				#tipo de llamada local
+				GraLog AFUMB INFO "TIPO DE LLAMADA LOC"				
+				echo "LOC"
+				return
+			else
+				#tipo de llamada invalida
+				GraLog AFUMB INFO "Tipo de llamada invalido"
+				echo "INV"
+				return
+			fi
+		fi
+	
+	fi
+}
+
+validarRegistro() {
+
+	#valido agente
+	reg=$1
+	agente=$(echo "$reg" | cut -d';' -f1)	
+	validaAgente=$(validarAgente $agente) 
+	if [ $validaAgente -eq 0 ]
+	then	
+		#valido area
+		area=$(echo "$reg" | cut -d';' -f4)
+		validaArea=$(validarArea $area)
+		if [ $validaArea -eq 0 ]
+		then
+			
+			#valido numero linea A
+			numerolinea=$(echo "$reg" | cut -d';' -f5)
+			validaNumeroLineaA=$(validarNumeroLineaA $area $numerolinea)
+			if [ $validaNumeroLineaA -eq 0 ]
+			then
+		
+				#valido el numero linea B
+				validaNumeroLineaB=$(validarNumeroLineaB "$reg")
+				if [ $validaNumeroLineaB -eq 0 ]
+				then	
+					#valido el tiempo de conversacion
+					validaTiempoConv=$(validarTiempoConversacion "$reg")	
+					if [ $validaTiempoConv -eq 0 ]
+					then
+							
+						echo 0
+						return
+					else
+						GraLog AFUMB INFO "Regisro invalido: $reg"
+						echo 1
+						return
+					fi
+				else
+					GraLog AFUMB INFO "Regisro invalido: $reg"
+					echo 1
+					return
+				fi 		
+				
+			else
+				GraLog AFUMB INFO "Regisro invalido: $reg"
+				echo 1
+				return
+			fi 		
+		else
+			GraLog AFUMB INFO "Regisro invalido: $reg"
+			echo 1
+			return
+		fi
+	else
+		GraLog AFUMB INFO "Regisro invalido: $reg"
+		echo 1
+		return
+	fi
+}
+
 
 ###Principal###
 
-MAE_COD_PAIS=$2
+#MAE_COD_PAIS=$2
+MAE_COD_PAIS="../master/CdP" #TODO-BORRAR ES SOLO PARA PROBAR
 #MAE_COD_AREA_ARG=$3
 MAE_COD_AREA_ARG="../master/CdA"  #TODO-BORRAR ES SOLO PARA PROBAR
 MAE_CENTRAL=$4
@@ -156,18 +409,21 @@ do
 	do
 		let contLeidos=$contLeidos+1
 		#echo "Linea a procesar: $linea"
-		validaRegistro=$(validarRegistro $linea)
-		if [ $validaRegistro -eq 0 ]
+		validaRegistro=$(validarRegistro "$linea")
+		if [ $validaRegistro -eq  0 ]
 		then
 			echo "registro valido"
 			#continuar	
 
-		fi 
+		else 
+			echo "rechazo registro"
+			#ir a rechazar registro
+		fi			
 	done < $path_origen
 	echo "fin proceso archivo"
 	path_destino_proc=$DIRECTORIO_PROC"/"$FILE
 	#archivo procesado
-	#MoverA $path_origen $path_destino_proc
+	MoverA $path_origen $path_destino_proc
 	GraLog AFUMB INFO "archivo procesado $FILE"
 
 done
