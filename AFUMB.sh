71d92998ca798ad05c6ffcc02c9aa60a212a339e
carolinarau <rau.carolina@gmail.com>
2015-10-12 16:20:20 -0300

    Nueva version donde acomode mejor las cosas y agregue los puntos del 6 al 10. Faltan puntos 4.3 , 4.4 y 4.5

--- AFUMB.sh
+++ AFUMB.sh
@@ -87,7 +87,7 @@ validarNumeroLineaB() {
 	if [ "$TIPO_LLAMADA" = "DDI" ]
 	then	
 		COD_AREA_PAIS=$(echo "$1" | cut -d';' -f6)
-		GraLog AFUMB INFO "Codigo area pais: $COD_AREA_PAIS"
+		#GraLog AFUMB INFO "Codigo area pais: $COD_AREA_PAIS"
 		if [ ! -z $COD_AREA_PAIS ]
 		then
 			COD_AREA_PAIS_OK=$(validarCodPais $COD_AREA_PAIS)
@@ -95,7 +95,7 @@ validarNumeroLineaB() {
 			then
 				#Como la llamada es DDI no tiene codigo de area
 				#valido la linea
-				GraLog AFUMB INFO "NUMLINEA: $NUMLINEA"
+				#GraLog AFUMB INFO "NUMLINEA: $NUMLINEA"
 				#Agregar expresion regular que valide que sea un numero - TODO
 				echo 0
 				return
@@ -110,7 +110,7 @@ validarNumeroLineaB() {
 		if [ "$TIPO_LLAMADA" = "DDN" ] || [ "$TIPO_LLAMADA" = "LOC" ]	
 		then
 			COD_AREA=$(echo "$1" | cut -d';' -f7)
-			GraLog AFUMB INFO "COD_AREA: $COD_AREA"
+			#GraLog AFUMB INFO "COD_AREA: $COD_AREA"
 			#Valido el codigo de area
 			COD_AREA_OK=$(validarArea $COD_AREA)
 			if [ $COD_AREA_OK -eq 0 ] 
@@ -119,11 +119,11 @@ validarNumeroLineaB() {
 				#Area valida		
 				#Tipo de llamada LOCAL o DDN			
 				AREA_LINEA=$COD_AREA$NUMLINEA
-				GraLog AFUMB INFO "AREA_LINEA: $AREA_LINEA"
+				#GraLog AFUMB INFO "AREA_LINEA: $AREA_LINEA"
 				LONG_AREA_LINEA=${#AREA_LINEA}
 				if [ $LONG_AREA_LINEA -eq 10 ]
 				then
-					GraLog AFUMB INFO "Linea B VALIDA: $NUMLINEA"
+					#GraLog AFUMB INFO "Linea B VALIDA: $NUMLINEA"
 					echo 0
 					return
 				else
@@ -219,16 +219,16 @@ determinarTipoLlamada() {
 	COD_AREA_PAIS=$(echo "$REGISTRO" | cut -d';' -f6)
 	if [ ! -z $COD_AREA_PAIS ]
 	then
-		GraLog AFUMB INFO "EXISTE CODIGO AREA PAIS"		
+		#GraLog AFUMB INFO "EXISTE CODIGO AREA PAIS"		
 		COD_AREA_PAIS_OK=$(validarCodPais $COD_AREA_PAIS)
-		GraLog AFUMB INFO "COD_AREA_PAIS: $COD_AREA_PAIS"
+		#GraLog AFUMB INFO "COD_AREA_PAIS: $COD_AREA_PAIS"
 		LINEA_B=$(echo "$REGISTRO" | cut -d';' -f8)
-		GraLog AFUMB INFO "LINEA_B: $LINEA_B"
-		GraLog AFUMB INFO "COD_AREA_PAIS_OK: $COD_AREA_PAIS_OK"
+		#GraLog AFUMB INFO "LINEA_B: $LINEA_B"
+		#GraLog AFUMB INFO "COD_AREA_PAIS_OK: $COD_AREA_PAIS_OK"
 		if [ $COD_AREA_PAIS_OK -eq  0 ]  &&  [ ! -z $LINEA_B ]
 		then
 			#tipo de llamada DDI
-			GraLog AFUMB INFO "TIPO DE LLAMADA DDI"	
+			#GraLog AFUMB INFO "TIPO DE LLAMADA DDI"	
 			echo "DDI"
 			return
 		else
@@ -236,7 +236,6 @@ determinarTipoLlamada() {
 			return 
 		fi
 	else
-		GraLog AFUMB INFO " NO EXISTE CODIGO AREA PAIS"
 		LINEA_A=$(echo "$REGISTRO" | cut -d';' -f5)
 		COD_AREA_A=$(echo "$REGISTRO" | cut -d';' -f4)
 		COD_AREA_B=$(echo "$REGISTRO" | cut -d';' -f7)
@@ -246,14 +245,14 @@ determinarTipoLlamada() {
 		if [ $COD_AREA_A -ne $COD_AREA_B ] && [ $LINEA_A_OK -eq 0 ]
 		then
 			#tipo de llamada DDN
-			GraLog AFUMB INFO "TIPO LLAMADA DDN"
+			#GraLog AFUMB INFO "TIPO LLAMADA DDN"
 			echo "DDN"
 			return
 		else
 			if [ $COD_AREA_A -eq $COD_AREA_B ] && [ $LINEA_A_OK -eq 0 ]
 			then
 				#tipo de llamada local
-				GraLog AFUMB INFO "TIPO DE LLAMADA LOC"				
+				#GraLog AFUMB INFO "TIPO DE LLAMADA LOC"				
 				echo "LOC"
 				return
 			else
@@ -326,8 +325,13 @@ validarRegistro() {
 	fi
 }
 
+#rechazarRegistro() {
+#}
 
-###Principal###
+
+###PRINCIPAL###
+
+#OBTENGO LOS PARAMETROS DE ENTRADA
 
 #MAE_COD_PAIS=$2
 MAE_COD_PAIS="../master/CdP" #TODO-BORRAR ES SOLO PARA PROBAR
@@ -346,84 +350,115 @@ DIRECTORIO_ACEP="$PWD/acep" #TODO- borrar es solo para probar.
 DIRECTORIO_PROC="$PWD/proc" #TODO- borrar es solo para probar.
 DIRECTORIO_RECH="$PWD/rech" #TODO- borrar es solo para probar.
 
-#Calculo la cantidad de arhivos a procesar.
+#CALCULO LA CANTIDAD DE ARCHIVOS A PROCESAR
 
-cant=$(ls $DIRECTORIO_ACEP | wc -l)
+CANT_TOTAL_ARCH=$(ls $DIRECTORIO_ACEP | wc -l)
 
 #INICIALIZO EL LOG
 
 GraLog AFUMB INFO "Inicio de AFUMB"
-GraLog AFUMB INFO "Cantidad de archivos a procesar: $cant"
+GraLog AFUMB INFO "Cantidad de archivos a procesar: $CANT_TOTAL_ARCH"
 
-#PROCESAR ARCHIVOS EN ORDEN CRONOLOGICO, DEL MAS ANTIGUO AL MAS NUEVO
-#1
+#CONTADORES
+CANT_ARCH_PROCESADOS=0
+CANT_ARCH_RECHAZADOS=0	
+CANT_LLAMADAS=0	
+CANT_LLAM_CON_UMBRAL=0
+CANT_LLAM_SIN_UMBRAL=0
+CANT_LLAM_SOSPECHOSAS=0
+CANT_LLAM_NO_SOSPECHOSAS=0	
+CANT_LLAM_RECHAZADAS=0
+
+#1 - PROCESAR ARCHIVOS EN ORDEN CRONOLOGICO, DEL MAS ANTIGUO AL MAS NUEVO
+
 for FILE in `ls $DIRECTORIO_ACEP`    #TODO - falta agregar que tome el archivo mas antiguo
 do
 	path_origen=$DIRECTORIO_ACEP"/"$FILE
-	GraLog AFUMB INFO "Proceso: $FILE"
-	echo "Proceso $FILE"
+	#GraLog AFUMB INFO "Proceso: $FILE"
 	arch=""
-	#2
-	#Verifico que el archivo no este duplicado
+	
+	#2 - Verifico que el archivo no este duplicado
 	ls -1 $DIRECTORIO_PROC > "$DIRECTORIO_PROC/nombres_archivos.tmp"
 
 	arch=`grep "$FILE" "$DIRECTORIO_PROC/nombres_archivos.tmp"`	
 
-	#echo "ARCHVO A BUSCAR $arch"
 	path_destino_rech=$DIRECTORIO_RECH"/"$FILE
 	
 	if [ "$arch" = "$FILE" ]
 	then
-		#archivo duplicado mover a rechazados
-		#echo "archivo duplicado: $FILE"
+		#ARCHIVO DUPLICADO MOVER A RECHAZADOS
 		MoverA $path_origen $path_destino_rech
 		GraLog AFUMB INFO "Se rechaza el archivo por estar duplicado: $FILE"
+		#INCREMENTO CONTADOR DE ARCHIVO RECHAZADO
+		CANT_ARCH_RECHAZADOS=$(($CANT_ARCH_RECHAZADOS+1))
 		exit 1
 	fi
 	
-	#Verifico la cantidad de campos del primer registro
+	#SE VERIFICA LA CANTIDAD DE CAMPOS DEL PRIMER REGISTRO
 	linea=$(head -1 $path_origen)
 	cant_de_campos=$((`echo "$linea" | grep -o ';' | wc -l` + 1))	
 	
 	if [ $cant_de_campos -ne 8 ]
 	then
-		#archivo con formato invalido
-		#echo "archivo formato invalido $FILE"		
+		#ARCHIVO CON FORMATO INVALIDO		
 		MoverA $path_origen $path_destino_rech
 		GraLog AFUMB INFO "Se rechaza el archivo porque su estrutura no se corresponde con el formato esperado: $FILE"
+		#INCREMENTO CONTADOR DE ARCHIVO RECHAZADO
+		CANT_ARCH_RECHAZADOS=$(($CANT_ARCH_RECHAZADOS+1))
 		exit 1
 	fi
 	
-	#3
+	#3 - ARCHIVO A PROCESAR
 	GraLog AFUMB INFO "Archivo a procesar: $FILE"
 	
-	#4
-	#PROCESAR UN REGISTRO
-	#VALIDAR LOS CAMPOS DEL REGISTRO
-
-	contLeidos=0
-	contAcep=0
-	contRech=0
-
+	#4 - PROCESAR UN REGISTRO
 	while read linea
 	do
-		let contLeidos=$contLeidos+1
-		#echo "Linea a procesar: $linea"
+		CANT_LLAMADAS=$(($CANT_LLAMADAS+1))
+		#VALIDAR LOS CAMPOS DEL REGISTRO
 		validaRegistro=$(validarRegistro "$linea")
 		if [ $validaRegistro -eq  0 ]
 		then
 			echo "registro valido"
-			#continuar	
+			#CONTINUAR CON EL PROCESO PARA DETERMINAR SI LA LLAMADA ES SOSPECHOZA	
 
 		else 
+			#SE RECHAZA EL REGISTRO
 			echo "rechazo registro"
-			#ir a rechazar registro
-		fi			
-	done < $path_origen
-	echo "fin proceso archivo"
-	path_destino_proc=$DIRECTORIO_PROC"/"$FILE
-	#archivo procesado
-	MoverA $path_origen $path_destino_proc
-	GraLog AFUMB INFO "archivo procesado $FILE"
+			GraLog AFUMB INFO "Registro invalido: $linea"
+			CANT_LLAM_RECHAZADAS=$(($CANT_LLAM_RECHAZADAS+1))
+			#CONTINUAR CON EL PROCESO rechazarRegistro
+		fi
 
+				
+	done < $path_origen
+
+	#6 - FIN DEL ARCHIVO	
+	path_destino_proc=$DIRECTORIO_PROC"/"$FILE
+	MoverA $path_origen $path_destino_proc
+	GraLog AFUMB INFO "Archivo procesado: $FILE"
+	
+	#TOTALES A GRABAR
+	GraLog AFUMB INFO "Cantidad de llamadas= $CANT_LLAMADAS: Rechazadas= $CANT_LLAM_RECHAZADAS Con umbral= $CANT_LLAM_CON_UMBRAL Sin umbral= $CANT_LLAM_SIN_UMBRAL"
+	GraLog AFUMB INFO "Cantidad de llamadas sospechosas= $CANT_LLAM_SOSPECHOSAS, No sospechosas= $CANT_LLAM_NO_SOSPECHOSAS"
+
+	#INCREMENTO CONTADOR DE ARCHIVO PROCESADO
+	CANT_ARCH_PROCESADOS=$(($CANT_ARCH_PROCESADOS+1))
+
+	#7 - INICIALIAZAR CONTADORES DE REGISTROS
+	CANT_LLAMADAS=0	
+	CANT_LLAM_CON_UMBRAL=0
+	CANT_LLAM_SIN_UMBRAL=0
+	CANT_LLAM_SOSPECHOSAS=0
+	CANT_LLAM_NO_SOSPECHOSAS=0	
+	CANT_LLAM_RECHAZADAS=0
 done
+
+#10 - FIN DEL PROCESO
+GraLog AFUMB INFO "Cantidad de archivos procesados: $CANT_ARCH_PROCESADOS"
+GraLog AFUMB INFO "Cantidad de archivos rechazados: $CANT_ARCH_RECHAZADOS"
+GraLog AFUMB INFO "Fin de AFUMB"
+
+
+
+
