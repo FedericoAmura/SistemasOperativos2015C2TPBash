f385625d8ea107f944fb87cfbb6a62fbbee79fc3
emanuelcondo <emanuelcondo@gmail.com>
2015-10-12 23:41:58 -0300

    f1 y f2 (perl- estadísticas- filtro central, filtro oficina) terminado

--- AFLIST.pl
+++ AFLIST.pl
@@ -313,11 +313,17 @@ while ($input ne '0')
 sub f_1_central_cantidad_llam_sosp
 {
 	print "llamando..f_1_central_cantidad_llam_sosp\n";
+	my ($h_central1, $h_central2) = &filtrarCentralPorCantidadDeLlamadasSospechosas;
 	if ($_[0] == "1"){
-	print "con filtro por cantidad de llamadas\n";	
+		print "con filtro por cantidad de llamadas\n";	
+		print "Central            Cantidad de llamadas\n";	
+		&imprimirFiltroCentral(%{$h_central1});
 	}
 	if ($_[0] == "2"){
-	print "con filtro por cantidad de segundos\n";	
+		print "con filtro por cantidad de segundos\n";
+		print "con filtro por cantidad de segundos\n";
+		print "Central            Cantidad de segundos\n";	
+		&imprimirFiltroCentral(%{$h_central2});	
 	}
 	
 }
@@ -325,11 +331,16 @@ sub f_1_central_cantidad_llam_sosp
 sub f_2_ofi_cantidad_llam_sosp
 {
 	print "llamando..f_2_ofi_cantidad_llam_sosp\n";
+	my ($h_oficina1, $h_oficina2) = &filtrarOficinaPorCantidadDeLlamadasSospechosas;
 	if ($_[0] == "1"){
-	print "con filtro por cantidad de llamadas\n";	
+		print "con filtro por cantidad de llamadas\n";
+		print "Oficina        Cantidad de llamadas\n";
+		&imprimirFiltroOficina(%{$h_oficina1});	
 	}
 	if ($_[0] == "2"){
-	print "con filtro por cantidad de segundos\n";	
+		print "con filtro por cantidad de segundos\n";
+		print "Oficina        Cantidad de segundos\n";
+		&imprimirFiltroOficina(%{$h_oficina2});		
 	}
 }
 
@@ -365,4 +376,146 @@ sub presione_tecla_para_continuar {
     print "Presione una tecla para continuar...\n";
     $key = <STDIN>;
     chomp($key);
+}
+####################################################
+# campos: (código_país; nombre_país)
+sub cargarCodigosDePais(){
+	my (%hash_CdP);
+	my (@registro);
+	open(ENTRADA,"<master/CdP") || die "ERROR: No se encontró archivo maestro CdP.\n";
+
+	while (my $linea = <ENTRADA>){
+		chomp($linea);
+		@registro = split(";",$linea); 
+		$hash_CdP{$registro[0]} = $registro[1];
+	}
+	close(ENTRADA);
+	return %hash_CdP;
+}
+
+# campos: (nombre_área; código_área)
+sub cargarCodigosDeArea(){
+	my (%hash_CdA);
+	my (@registro);
+	open(ENTRADA,"<master/CdA") || die "ERROR: No se encontró archivo maestro CdA.\n";
+
+	while (my $linea = <ENTRADA>){
+		chomp($linea);
+		@registro = split(";",$linea); 
+		$hash_CdA{$registro[1]} = $registro[0];
+	}
+	close(ENTRADA);
+	return %hash_CdA;
+}
+
+# campos: (código_central; descripción)
+sub cargarCodigosDeCentrales(){
+	my (%hash_CdC);
+	my (@registro);
+	open(ENTRADA,"<master/CdC") || die "ERROR: No se encontró archivo maestro CdC.\n";
+
+	while (my $linea = <ENTRADA>){
+		chomp($linea);
+		@registro = split(";",$linea); 
+		$hash_CdC{$registro[0]} = $registro[1];
+	}
+	return %hash_CdC;
+}
+
+# campos: (id_agente; nombre_agente; apellido_agente; oficina; email)
+sub cargarCodigosDeAgentes(){
+	my (%hash_agentes);
+	my (@info_agentes);
+	open(ENTRADA,"<master/agentes") || die "ERROR: No se encontró archivo maestro agentes.\n";
+
+	while (my $linea = <ENTRADA>){
+		chomp($linea);
+		@info_agentes = split(";",$linea); 
+		$hash_agentes{$info_agentes[0]} = @info_agentes;
+	}
+	return %hash_agentes;
+}
+
+# Central con mayor cantidad de llamadas sospechosas (cantidad ó tiempo)
+# Devuelve un arreglo con 2 hashes (cantidad ó tiempo)
+sub filtrarCentralPorCantidadDeLlamadasSospechosas(){
+	# 2 hashes para guardar por cantidades(#) y por segundos(tiempo) de llamadas sospechosas.
+	my (%hash_centrales_cant, %hash_centrales_seg);
+	my ($dir) = "proc"; # esta hardcodeado, debería ir $PROCDIR
+
+	if (opendir(DIRH,"$dir")){
+		@archivos=readdir(DIRH);
+		closedir(DIRH);
+	}
+
+	foreach my $file (@archivos){
+		next if ($file eq "." || $file eq "..");
+		open(ARCH,"<$dir/$file") || die "ERROR: No se pudo abrir el archivo $file.\n";
+
+		# Para cada registro del archivo, tomamos el campo "id_central" y "tiempoConversación"
+		# y actualizamos los hashes
+		while (my $registro = <ARCH>) {
+			chomp($registro);
+			my (@a_registro) = split(";",$registro);
+			$hash_centrales_cant{$a_registro[0]} += 1;
+			$hash_centrales_seg{$a_registro[0]} += $a_registro[4];
+		}
+		close(ARCH);
+	}
+	# Se van a devolver los 2 hashes en un vector (por una cuestión de eficiencia)
+	return (\%hash_centrales_cant, \%hash_centrales_seg);
+}
+
+# Oficina con mayor cantidad de llamadas sospechosas (cantidad ó tiempo)
+# Devuelve un arreglo con 2 hashes (cantidad ó tiempo)
+sub filtrarOficinaPorCantidadDeLlamadasSospechosas(){
+	# 2 hashes para guardar por cantidades(#) y por segundos(tiempo) de llamadas sospechosas.
+	my (%hash_oficina_cant, %hash_oficina_seg);
+	my ($dir) = "proc"; # esta hardcodeado, debería ir $PROCDIR
+
+	if (opendir(DIRH,"$dir")){
+		@archivos=readdir(DIRH);
+		closedir(DIRH);
+	}
+
+	foreach my $file (@archivos){
+		next if ($file eq "." || $file eq "..");
+		open(ARCH,"<$dir/$file") || die "ERROR: No se pudo abrir el archivo $file.\n";
+		
+		# En el nombre del archivo esta el identificador de la oficina
+		@campos = split("_",$file);
+		$hash_oficina_cant{$campos[0]} += 1;
+
+		# Para todos los registros del archivo, tomo el tiempo de conversación
+		while (my $registro = <ARCH>) {
+			chomp($registro);
+			my @a_registro = split(";",$registro);
+			$hash_oficina_seg{$a_registro[0]} += $a_registro[4];
+		}
+		close(ARCH);
+	}
+	# Se van a devolver los 2 hashes en un vector (por una cuestión de eficiencia)
+	return (\%hash_oficina_cant, \%hash_oficina_seg);
+}
+
+sub imprimirFiltroCentral(){
+	# El primero hash contiene para cada id un valor acumulado (ej: [id_central, cant_llamadas])
+	# El segundo hash contiene para cada id el valor propiamente dicho (ej: [id_central, central])
+	my (%h_data) = @_;
+	my (@claves) = keys(%h_data);
+	my (%h_info) = cargarCodigosDeCentrales;
+
+	foreach my $key (@claves){
+		print $h_info{$key}."      ".$h_data{$key}."\n";
+	}
+}
+
+sub imprimirFiltroOficina(){
+	# El primero hash contiene para cada id un valor acumulado (ej: [id_central, cant_llamadas])
+	my (%h_data) = @_;
+	my (@claves) = keys(%h_data);
+
+	foreach my $key (@claves){
+		print $key."            ".$h_data{$key}."\n";
+	}
 }
--- aflist.pl
+++ aflist.pl
@@ -4,7 +4,7 @@
 # campos: (código_país; nombre_país)
 sub cargarCodigosDePais(){
 	my (%hash_CdP);
-	open(ENTRADA,"<CdP") || die "ERROR: No se encontró archivo maestro CdP.\n";
+	open(ENTRADA,"<master/CdP") || die "ERROR: No se encontró archivo maestro CdP.\n";
 
 	while ($linea = <ENTRADA>){
 		chomp($linea);
@@ -18,12 +18,12 @@ sub cargarCodigosDePais(){
 # campos: (nombre_área; código_área)
 sub cargarCodigosDeArea(){
 	my (%hash_CdA);
-	open(ENTRADA,"<CdA") || die "ERROR: No se encontró archivo maestro CdA.\n";
+	open(ENTRADA,"<master/CdA") || die "ERROR: No se encontró archivo maestro CdA.\n";
 
 	while ($linea = <ENTRADA>){
 		chomp($linea);
 		@registro = split(";",$linea); 
-		$hash_CdP{$registro[1]} = $registro[0];
+		$hash_CdA{$registro[1]} = $registro[0];
 	}
 	close(ENTRADA);
 	return %hash_CdA;
@@ -32,12 +32,12 @@ sub cargarCodigosDeArea(){
 # campos: (código_central; descripción)
 sub cargarCodigosDeCentrales(){
 	my (%hash_CdC);
-	open(ENTRADA,"<CdC") || die "ERROR: No se encontró archivo maestro CdC.\n";
+	open(ENTRADA,"<master/CdC") || die "ERROR: No se encontró archivo maestro CdC.\n";
 
 	while ($linea = <ENTRADA>){
 		chomp($linea);
 		@registro = split(";",$linea); 
-		$hash_CdP{$registro[0]} = $registro[1];
+		$hash_CdC{$registro[0]} = $registro[1];
 	}
 	return %hash_CdC;
 }
@@ -46,7 +46,7 @@ sub cargarCodigosDeCentrales(){
 sub cargarCodigosDeAgentes(){
 	my (%hash_agentes);
 	my (@info_agentes);
-	open(ENTRADA,"<agentes") || die "ERROR: No se encontró archivo maestro agentes.\n";
+	open(ENTRADA,"<master/agentes") || die "ERROR: No se encontró archivo maestro agentes.\n";
 
 	while ($linea = <ENTRADA>){
 		chomp($linea);
@@ -59,9 +59,7 @@ sub cargarCodigosDeAgentes(){
 sub filtrarCentralPorCantidadDeLlamadasSospechosas(){
 	# 2 hashes para guardar por cantidades(#) y por segundos(tiempo) de llamadas sospechosas.
 	my (%hash_centrales_cant, %hash_centrales_seg);
-	my ($dir) = "proc";
-	my ($registro);
-	my (@a_registro);
+	my ($dir) = "proc"; # esta hardcodeado, debería ir $PROCDIR
 
 	if (opendir(DIRH,"$dir")){
 		@archivos=readdir(DIRH);
@@ -69,9 +67,12 @@ sub filtrarCentralPorCantidadDeLlamadasSospechosas(){
 	}
 
 	foreach my $file (@archivos){
-		open(ARCH,"<$file") || die "ERROR: No se pudo abrir el archivo $file.\n";
+		next if ($file eq "." || $file eq "..");
+		open(ARCH,"<$dir/$file") || die "ERROR: No se pudo abrir el archivo $file.\n";
 
-		while ($registro = <ARCH>) {
+		# Para cada registro del archivo, tomamos el campo "id_central" y "tiempoConversación"
+		# y actualizamos los hashes
+		while (my $registro = <ARCH>) {
 			chomp($registro);
 			my (@a_registro) = split(";",$registro);
 			$hash_centrales_cant{$a_registro[0]} += 1;
@@ -79,8 +80,8 @@ sub filtrarCentralPorCantidadDeLlamadasSospechosas(){
 		}
 		close(ARCH);
 	}
-	my @arrayHash = (%hash_centrales_cant, %hash_centrales_seg);
-	return @arrayHash;
+	# Se van a devolver los 2 hashes en un vector (por una cuestión de eficiencia)
+	return (\%hash_centrales_cant, \%hash_centrales_seg);
 }
 
 sub filtrarOficinaPorCantidadDeLlamadasSospechosas(){
@@ -94,23 +95,53 @@ sub filtrarOficinaPorCantidadDeLlamadasSospechosas(){
 	}
 
 	foreach my $file (@archivos){
-		open(ARCH,"<$file") || die "ERROR: No se pudo abrir el archivo $file.\n";
+		next if ($file eq "." || $file eq "..");
+		open(ARCH,"<$dir/$file") || die "ERROR: No se pudo abrir el archivo $file.\n";
 
+		# En el nombre del archivo esta el identificador de la oficina
+		@campos = split("_",$file);
+		$hash_oficina_cant{$campos[0]} += 1;
+
+		# Para todos los registros del archivo, tomo el tiempo de conversación
 		while (my $registro = <ARCH>) {
 			chomp($registro);
 			my @a_registro = split(";",$registro);
-			$hash_oficina_cant{$a_registro[0]} += 1;
 			$hash_oficina_seg{$a_registro[0]} += $a_registro[4];
 		}
 		close(ARCH);
 	}
-	my @arrayHash = (%hash_centrales_cant, %hash_centrales_seg);
-	return @arrayHash;
+	# Se van a devolver los 2 hashes en un vector (por una cuestión de eficiencia)
+	return (\%hash_oficina_cant, \%hash_oficina_seg);
 }
 
-print "hola\n";
-(%hash_por_cant_llam, %hash_por_tiempo) = filtrarOficinaPorCantidadDeLlamadasSospechosas;
-@keys1 = keys(%hash_por_cant_llam);
-foreach (@keys1){
-	print $_."  ".$hash_por_cant_llam{$_}."\n";
+sub imprimirFiltroCentral(){
+	# El primero hash contiene para cada id un valor acumulado (ej: [id_central, cant_llamadas])
+	# El segundo hash contiene para cada id el valor propiamente dicho (ej: [id_central, central])
+	my (%h_data) = @_;
+	my (@claves) = keys(%h_data);
+	my (%h_info) = cargarCodigosDeCentrales;
+
+	foreach my $key (@claves){
+		print $h_info{$key}."  ".$key."  ".$h_data{$key}."\n";
+	}
 }
+
+sub imprimirFiltroOficina(){
+	# El primero hash contiene para cada id un valor acumulado (ej: [id_central, cant_llamadas])
+	# El segundo hash contiene para cada id el valor propiamente dicho (ej: [id_central, central])
+	my (%h_data) = @_;
+	my (@claves) = keys(%h_data);
+
+	foreach my $key (@claves){
+		print "Oficina: ".$key."        ".$h_data{$key}."\n";
+	}
+}
+
+# MAIN
+print "Filtro por Central\n";
+($h_central1, $h_central2) = &filtrarCentralPorCantidadDeLlamadasSospechosas;
+&imprimirFiltroCentral(%{$h_central1});
+print "\n";
+print "Filtro por Oficina\n";
+($h_oficina1, $h_oficina2) = filtrarOficinaPorCantidadDeLlamadasSospechosas;
+&imprimirFiltroOficina(%{$h_oficina1});
